6fb9561da312a7895b629e19a51c49daee81d7ec
raju-kolati2323 <raju.kolati5@gmail.com>
2024-09-28 15:49:37 +0530

    second

--- server.js
+++ server.js
@@ -25,19 +25,19 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions={
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "DELETE", "PUT"],
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "Cache-Control",
-    "Expires",
-    "Pragma",
-  ],
-  credentials: true,
-}
-app.use(cors(corsOptions));
+// const corsOptions={
+//   origin: "http://localhost:5173",
+//   methods: ["GET", "POST", "DELETE", "PUT"],
+//   allowedHeaders: [
+//     "Content-Type",
+//     "Authorization",
+//     "Cache-Control",
+//     "Expires",
+//     "Pragma",
+//   ],
+//   credentials: true,
+// }
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
