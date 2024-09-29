ead4803fe11ef55fe1ca6c596b03e79236978596
raju-kolati2323 <raju.kolati5@gmail.com>
2024-09-29 20:08:16 +0530

    first commit

--- server.js
+++ server.js
@@ -25,19 +25,20 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// const corsOptions={
-//   origin: "http://localhost:5173",
-//   methods: ["GET", "POST", "DELETE", "PUT"],
-//   allowedHeaders: [
-//     "Content-Type",
-//     "Authorization",
-//     "Cache-Control",
-//     "Expires",
-//     "Pragma",
-//   ],
-//   credentials: true,
-// }
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173","https://ecommerce-deploy-0.web.app"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
