d58ccd4fa4bcdcb739e60dab055df75e553d8b59
raju-kolati2323 <raju.kolati5@gmail.com>
2024-09-29 20:35:32 +0530

    second commit

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://ecommerce-deploy-0.web.app"],
+    origin: ["http://localhost:5173","https://ecommerce-deploy-1.web.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
