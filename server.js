668b7c83f4d8d8552f79e9d94feb5fc893e68a3a
raju-kolati2323 <raju.kolati5@gmail.com>
2024-10-18 18:08:08 +0530

    web03

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://ecommerce-web02.web.app"],
+    origin: ["http://localhost:5173","https://ecommerce-web03.web.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
